8eab8386b503b1a93002ff079315a1313c66ba3e
wanglin208832 <big1lin@sina.com>
2022-07-19 20:27:14 +0800

    jest example01

---
+++ jestTest/example01.js
@@ -0,0 +1,5 @@
+function fun (a,b){
+    return a+b
+}
+
+module.exports = fun
---
+++ jestTest/example01.test.js
@@ -0,0 +1,5 @@
+var fun = require('./example01.js')
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(fun(1, 2)).toBe(3);
+  });
